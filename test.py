5a3ec21ccaf38a759a3e9b82e383c4b8c5ada282
Sharimiqbal <Codester8711@gmail.com>
2023-07-11 20:47:57 +0530

    Added division method

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print("Hello World")
+# Version 1.0.3
+class Calculator:
+
+    # def __init__(self, a, b): # Constructor
+    #     pass
+
+    def add(self, num1, num2):
+        return num1 + num2
+    
+    def sub(self, num1, num2):
+        return num1 - num2
+
+    def mul(self, num1, num2):
+        return num1 * num2
+
+    def div(self, num1, num2):
+        return num1 / num2
+
+
+
+cal1 = Calculator()
+print(cal1.add(12, 13))
